f289ceb54d494effec4b5aed506c96f47085928e
Timo Heddes <timo@driebit.nl>
2017-10-28 14:39:51 +0200

    Implemented router scroll behaviour

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,12 @@ const router = new Router({
     base: __dirname,
     transitionOnLoad: true,
     linkActiveClass: 'active',
+    scrollBehavior (to, from, savedPosition) {
+        const {focus} = to.params
+        focus && document.getElementById(focus) && document.getElementById(focus).scrollIntoView({ 
+            behavior: 'smooth' 
+        })
+    },
     routes
 })
 
@@ -18,10 +24,6 @@ const router = new Router({
 router.beforeResolve((to, from, next) => {
     document.title = `${config.site.name} - ${to.meta.title}`
     next()
-})
-
-router.afterEach((to, from) => {
-    // ...
 })
 
 export default router
